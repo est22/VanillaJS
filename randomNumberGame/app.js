1f3556d449de689b86f0b5e70302728520be033e
est22 <anlia.seoul@gmail.com>
2021-07-06 02:24:05 +0900

    add greetings

--- randomNumberGame/app.js
+++ randomNumberGame/app.js
@@ -1,9 +1,18 @@
+const loginForm = document.querySelector("#login-form"); // getElementById("login-form")
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-const h1 = document.querySelector("h1");
+const HIDDEN_CLASSNAME = "hidden"; // string만 포함된 변수는 대문자로 표기
 
-function handleTitleClick(){
-  // toggle  : class name 존재 확인 후 있으면 제거, 없으면 추가해줌
-  h1.classList.toggle("clicked");
+function onLoginSubmit(event){
+  event.preventDefault(); // 기본동작 실행 방지
+  loginForm.classList.add(HIDDEN_CLASSNAME); // hide the form
+  const username = loginInput.value; // save info
+  greeting.innerText = `Hello ${username}`; // put that name into h1, combine the variables
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+
+
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("submit", onLoginSubmit);
