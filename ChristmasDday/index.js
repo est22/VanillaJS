5718537abde2755a5add436bdce84d5fe87191a2
est22 <anlia.seoul@gmail.com>
2021-07-07 23:15:39 +0900

    deleted comments

--- ChristmasDday/index.js
+++ ChristmasDday/index.js
@@ -10,10 +10,6 @@ const clock = setInterval(function () {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    
-    // const hours = String(date.getHours()).padStart(2,"0");
-    // const minutes = String(date.getMinutes()).padStart(2,"0");
-    // const seconds = String(date.getSeconds()).padStart(2,"0");
     clockTitle.innerText=`${days}d ${hours}h ${minutes}m ${seconds}s`;
     
     
