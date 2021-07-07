31bbeca6aaf95237776b8bb9e35ba3df2d140ff1
est22 <anlia.seoul@gmail.com>
2021-07-07 22:43:08 +0900

    added padding

--- clock.js
+++ clock.js
@@ -2,8 +2,12 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText=`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText=`${hours}:${minutes}:${seconds}`;
 }
 
 getClock(); // 웹사이트가 실행되자마자 실행
 setInterval(getClock,1000); // 시간마다 반복하면서 실행
+
